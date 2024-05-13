c02a55e6f575b03367891335a3cfc3c31e50b529
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-05-13 08:17:17 +0700

    add

---
+++ application/logs/log-2024-05-10.php
@@ -0,0 +1,3 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2024-05-10 10:56:58 --> Language file contains no data: language/indonesian/form_validation_lang.php
