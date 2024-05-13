3292587245061b6c380741937ee2070fa86341b7
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-05-13 08:51:23 +0700

    Update Router.php

--- system/core/Router.php
+++ system/core/Router.php
@@ -122,6 +122,7 @@ class CI_Router {
 	 * @param	array	$routing
 	 * @return	void
 	 */
+	#[AllowDynamicProperties]
 	public function __construct($routing = NULL)
 	{
 		$this->config =& load_class('Config', 'core');
